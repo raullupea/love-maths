e496089ef371e24fc2924c7aa24395f4712d0ffd
raullupea <>
2023-12-18 20:53:48 +0000

    Addd favicon ,and final tidy ups for the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
         });
     }
+
+    document.getElementById("answer-box").addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    });
     runGame("addition");
 
 });
@@ -25,6 +31,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function runGame(gameType) {
 
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
+
     // Create two random numbers between 1 and 25 
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
@@ -33,7 +42,7 @@ function runGame(gameType) {
         displayAdditionQuestion(num1, num2);
 
     } else if (gameType === "subtract") {
-        displaySubtractQuestion(num1 ,num2);
+        displaySubtractQuestion(num1, num2);
     }
     else if (gameType === "multiply") {
         displayMultiplyQuestion(num1, num2);
@@ -80,7 +89,7 @@ function calculateCorrectAnswer() {
     } else if (operator === "x") {
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
-        return [operand1 - operand2 , "subtract"];
+        return [operand1 - operand2, "subtract"];
     }
     else {
         alert(`Unimplemented operator ${operator}`);
@@ -116,12 +125,12 @@ function displayAdditionQuestion(operand1, operand2) {
 
 }
 
-function displaySubtractQuestion(operand1 , operand2) {
+function displaySubtractQuestion(operand1, operand2) {
 
     document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
     document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
     document.getElementById('operator').textContent = "-";
-    
+
 
 
 }
